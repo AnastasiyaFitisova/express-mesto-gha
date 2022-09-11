78045d48629733b9d6a4975deb01caf66b139e3c
Anastasiya Fitisova <Anastasiya.Fitisova@yandex.ru>
2022-09-11 19:39:40 +0300

    исправить ошибки автотестов

--- app.js
+++ app.js
@@ -21,6 +21,21 @@ app.use(userRoutes);
 
 app.use(cardRoutes);
 
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({
+    message: statusCode === 500
+      ? 'На сервере произошла ошибка'
+      : message,
+  });
+  next(err);
+});
+
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/mestodb', {
     useNewUrlParser: true,
