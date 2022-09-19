f06c56b44f8dd898d2c2b69cbc9a86897c76c6be
Anastasiya Fitisova <Anastasiya.Fitisova@yandex.ru>
2022-09-19 20:14:25 +0300

    исправить замечания ревью от 19.09.22

--- models/user.js
+++ models/user.js
@@ -4,21 +4,18 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: [2, 'name must be at least 2 characters'],
     maxlength: [30, 'link must be max 30 characters'],
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minlength: [2, 'row about must be at least 2 characters'],
     maxlength: [30, 'row about must be max 30 characters'],
     default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator(v) {
